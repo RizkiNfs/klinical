63cff9fbe1c4d6ea2c79f0966d404029a39a92b5
rizki <rizki.nofsyiami@gmail.com>
2023-06-21 11:38:23 +0700

    feat(auth): redirect to app if already login

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
+<script setup lang="ts">
+
+definePageMeta({
+  middleware: () => {
+    const token = useCookie('token')
+    if(token.value) {
+      return navigateTo('/app')
+    }
+  }
+})
+
+</script>
+
 <template>
   <div class="flex h-screen">
     <div class="w-1/2 h-full bg-teal-50" />
     <div class="w-1/2 h-full flex items-center justify-center">
       <div class="w-[480px]">
-        <!-- <el-form /> -->
         <form-login />
       </div>
     </div>
